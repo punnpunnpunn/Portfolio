bb02d77c69b7d0bd2464d50e53bc6e604fb211b3
punnpunnpunn <86233160+punnpunnpunn@users.noreply.github.com>
2025-05-09 17:42:37 +0700

    first commit

---
+++ backend/schemaTypes/index.js
@@ -0,0 +1,14 @@
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+// Then we give our schema to the builder and provide the result to Sanity
+export const schemaTypes = [
+    works, testimonials, brands, abouts, skills, workExperience, experiences, contact
+]
