419bc26837d5b0cd5cfaff377e85ba9cb4c94712
punnpunnpunn <86233160+punnpunnpunn@users.noreply.github.com>
2025-06-12 22:17:02 +0700

    Home page done for now

--- frontend/src/container/About.jsx
+++ frontend/src/container/About.jsx
@@ -1,7 +1,12 @@
 function About() {
-    return <h1>
-        About
-    </h1>
+    return <div className="about">
+        <h1>About me</h1>
+        <p>
+            Hi there! My full name is Punnawit Payapvattanavong but you can call me Punpun.
+            I'm orignially from Thailand and am currently studying Computer Science and Mathematical Applications in Economics and Finance at the University of Toronto.
+            I love playing chess, acting, singing, and running. I also love talking to people so feel free to hit me up!
+        </p>
+    </div>
 }
 
 export default About
--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,15 +1,11 @@
-import picture from '../assets/my_pic.png'
-
 function Home() {
     return <div className='home'>
         <div className='text'>
             <h3>Hello there! I'm</h3>
             <h1>Punpun (Punnawit Payapvattanavong) </h1>
             <br/><br/>
-            <h4>Computer Science Major | Mathematical Applications in Economics and Finance Specialist | Statistics Minor @ University of Toronto</h4>
-            <footer>[Contacts here]</footer>
+            <p>Computer Science Major | Mathematical Applications in Economics and Finance Specialist | Statistics Minor @ University of Toronto</p>
         </div>
-        {/*<img src= {picture} alt="Punpun's Picture"/>*/}
     </div>
 }
 
